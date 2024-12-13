85fe291a0bb8cc364a1016d0b77d1e6fe7161220
luozhang <luozhang@wafersystems.com>
2024-12-13 11:30:23 +0800

    memorix first commit

---
+++ internal/service/service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"memorix/internal/repository"
+	"memorix/pkg/jwt"
+	"memorix/pkg/log"
+	"memorix/pkg/sid"
+)
+
+type Service struct {
+	logger *log.Logger
+	sid    *sid.Sid
+	jwt    *jwt.JWT
+	tm     repository.Transaction
+}
+
+func NewService(
+	tm repository.Transaction,
+	logger *log.Logger,
+	sid *sid.Sid,
+	jwt *jwt.JWT,
+) *Service {
+	return &Service{
+		logger: logger,
+		sid:    sid,
+		jwt:    jwt,
+		tm:     tm,
+	}
+}
