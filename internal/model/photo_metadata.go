4213e5594d6e9065e16fcb57d0cc10c37fb79f46
magiclz233 <magiclz233@163.com>
2025-04-23 22:48:16 +0800

    feat(model): 添加PhotoMetadata模型用于存储照片元数据
    添加了PhotoMetadata模型，用于存储照片的元数据信息，包括描述、相机型号、制造商、镜头型号、拍摄日期、曝光时间、光圈值、ISO值、焦距、闪光灯状态、方向、曝光程序、GPS纬度和GPS经度等字段。该模型将用于后续的照片管理功能。

---
+++ internal/model/photo_metadata.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type PhotoMetadata struct {
+	gorm.Model
+	Description     *string    `gorm:"column:description;comment:描述" json:"description"`
+	Camera          *string    `gorm:"column:camera;comment:相机型号" json:"camera"`
+	Maker           *string    `gorm:"column:maker;comment:制造商" json:"maker"`
+	Lens            *string    `gorm:"column:lens;comment:镜头型号" json:"lens"`
+	DateShot        *time.Time `gorm:"column:date_shot;comment:拍摄日期" json:"date_shot"`
+	Exposure        *float64   `gorm:"column:exposure;comment:曝光时间" json:"exposure"`
+	Aperture        *float64   `gorm:"column:aperture;comment:光圈值" json:"aperture"`
+	Iso             *int64     `gorm:"column:iso;comment:ISO值" json:"iso"`
+	FocalLength     *float64   `gorm:"column:focal_length;comment:焦距" json:"focal_length"`
+	Flash           *int64     `gorm:"column:flash;comment:闪光灯状态" json:"flash"`
+	Orientation     *int64     `gorm:"column:orientation;comment:方向" json:"orientation"`
+	ExposureProgram *int64     `gorm:"column:exposure_program;comment:曝光程序" json:"exposure_program"`
+	GPSLatitude     *float64   `gorm:"column:gps_latitude;comment:GPS纬度" json:"gps_latitude"`
+	GPSLongitude    *float64   `gorm:"column:gps_longitude;comment:GPS经度" json:"gps_longitude"`
+}
+
+
