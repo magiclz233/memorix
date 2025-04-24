a403b632e121fda456aeff15a9b78a2caea22dbb
magiclz233 <magiclz233@163.com>
2025-04-24 22:58:04 +0800

    feat(model): 在PhotoMetadata中添加图片宽度、高度和白平衡字段
    为了支持更详细的图片元数据信息，新增了`ResolutionWidth`、`ResolutionHeight`和`WhiteBalance`字段，分别用于存储图片的宽度、高度和白平衡信息。

--- internal/model/photo_metadata.go
+++ internal/model/photo_metadata.go
@@ -8,20 +8,21 @@ import (
 
 type PhotoMetadata struct {
 	gorm.Model
-	Description     *string    `gorm:"column:description;comment:描述" json:"description"`
-	Camera          *string    `gorm:"column:camera;comment:相机型号" json:"camera"`
-	Maker           *string    `gorm:"column:maker;comment:制造商" json:"maker"`
-	Lens            *string    `gorm:"column:lens;comment:镜头型号" json:"lens"`
-	DateShot        *time.Time `gorm:"column:date_shot;comment:拍摄日期" json:"date_shot"`
-	Exposure        *float64   `gorm:"column:exposure;comment:曝光时间" json:"exposure"`
-	Aperture        *float64   `gorm:"column:aperture;comment:光圈值" json:"aperture"`
-	Iso             *int64     `gorm:"column:iso;comment:ISO值" json:"iso"`
-	FocalLength     *float64   `gorm:"column:focal_length;comment:焦距" json:"focal_length"`
-	Flash           *int64     `gorm:"column:flash;comment:闪光灯状态" json:"flash"`
-	Orientation     *int64     `gorm:"column:orientation;comment:方向" json:"orientation"`
-	ExposureProgram *int64     `gorm:"column:exposure_program;comment:曝光程序" json:"exposure_program"`
-	GPSLatitude     *float64   `gorm:"column:gps_latitude;comment:GPS纬度" json:"gps_latitude"`
-	GPSLongitude    *float64   `gorm:"column:gps_longitude;comment:GPS经度" json:"gps_longitude"`
+	Description      *string    `gorm:"column:description;comment:描述" json:"description"`
+	Camera           *string    `gorm:"column:camera;comment:相机型号" json:"camera"`
+	Maker            *string    `gorm:"column:maker;comment:制造商" json:"maker"`
+	Lens             *string    `gorm:"column:lens;comment:镜头型号" json:"lens"`
+	DateShot         *time.Time `gorm:"column:date_shot;comment:拍摄日期" json:"date_shot"`
+	Exposure         *float64   `gorm:"column:exposure;comment:曝光时间" json:"exposure"`
+	Aperture         *float64   `gorm:"column:aperture;comment:光圈值" json:"aperture"`
+	Iso              *int64     `gorm:"column:iso;comment:ISO值" json:"iso"`
+	FocalLength      *float64   `gorm:"column:focal_length;comment:焦距" json:"focal_length"`
+	Flash            *int64     `gorm:"column:flash;comment:闪光灯状态" json:"flash"`
+	Orientation      *int64     `gorm:"column:orientation;comment:方向" json:"orientation"`
+	ExposureProgram  *int64     `gorm:"column:exposure_program;comment:曝光程序" json:"exposure_program"`
+	GPSLatitude      *float64   `gorm:"column:gps_latitude;comment:GPS纬度" json:"gps_latitude"`
+	GPSLongitude     *float64   `gorm:"column:gps_longitude;comment:GPS经度" json:"gps_longitude"`
+	ResolutionWidth  *int        `gorm:"column:resolution_width;comment:图片宽度" json:"resolution_width"`
+	ResolutionHeight *int        `gorm:"column:resolution_height;comment:图片高度" json:"resolution_height"`
+	WhiteBalance     *string    `gorm:"column:white_balance;comment:白平衡" json:"white_balance"`
 }
-
-
