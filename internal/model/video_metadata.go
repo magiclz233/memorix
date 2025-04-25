1736de64ce351982e452c723471d0d3917655d6c
magiclz233 <magiclz233@163.com>
2025-04-25 22:33:52 +0800

    style(model): 格式化视频元数据结构体代码
    对齐结构体字段的缩进，提升代码可读性

--- internal/model/video_metadata.go
+++ internal/model/video_metadata.go
@@ -3,15 +3,13 @@ package model
 import "gorm.io/gorm"
 
 type VideoMetadata struct {
-    gorm.Model
-	ResolutionWidth        int     `gorm:"not null"`
-	ResolutionHeight       int     `gorm:"not null"`
-	Duration     float64 `gorm:"not null"`
-	Codec        *string
-	Framerate    *float64
-	Bitrate      *string
-	ColorProfile *string
-	Audio        *string
+	gorm.Model
+	ResolutionWidth  int     `gorm:"not null"`
+	ResolutionHeight int     `gorm:"not null"`
+	Duration         float64 `gorm:"not null"`
+	Codec            *string
+	Framerate        *float64
+	Bitrate          *string
+	ColorProfile     *string
+	Audio            *string
 }
-
-
