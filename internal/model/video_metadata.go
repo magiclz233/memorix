a83bf43353a4e4bcae47fa6693e746ed68dacf9c
magiclz233 <magiclz233@163.com>
2025-04-25 22:34:28 +0800

    refactor(model): 为VideoMetadata结构体添加列注释和JSON标签
    为VideoMetadata结构体的字段添加了GORM列注释和JSON标签，以提高代码的可读性和序列化支持。

--- internal/model/video_metadata.go
+++ internal/model/video_metadata.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type VideoMetadata struct {
 	gorm.Model
-	ResolutionWidth  int     `gorm:"not null"`
-	ResolutionHeight int     `gorm:"not null"`
-	Duration         float64 `gorm:"not null"`
-	Codec            *string
-	Framerate        *float64
-	Bitrate          *string
-	ColorProfile     *string
-	Audio            *string
+	ResolutionWidth  int      `gorm:"column:resolution_width;comment:视频宽度;not null" json:"resolution_width"`
+	ResolutionHeight int      `gorm:"column:resolution_height;comment:视频高度;not null" json:"resolution_height"`
+	Duration         float64  `gorm:"column:duration;comment:视频时长;not null" json:"duration"`
+	Codec            *string  `gorm:"column:codec;comment:视频编码格式" json:"codec"`
+	Framerate        *float64 `gorm:"column:framerate;comment:帧率" json:"framerate"`
+	Bitrate          *string  `gorm:"column:bitrate;comment:比特率" json:"bitrate"`
+	ColorProfile     *string  `gorm:"column:color_profile;comment:色彩配置" json:"color_profile"`
+	Audio            *string  `gorm:"column:audio;comment:音频信息" json:"audio"`
 }
