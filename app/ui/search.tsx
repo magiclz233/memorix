f1eef77ea2c692435b736889710b8c58bc323fa6
magiclz233 <magiclz233@163.com>
2025-12-09 18:34:01 +0800

    feat(发票): 实现发票的创建、编辑和删除功能
    - 添加创建发票的表单和页面
    - 实现编辑发票功能及页面
    - 添加删除发票功能
    - 为发票添加分页功能
    - 添加搜索功能并优化性能
    - 添加404页面处理未找到的发票

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,21 +2,26 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter  } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(val: string) {
+
+  const handleSearch = useDebouncedCallback((val: string) => {
+     console.log(`Searching... ${val}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (val) {
       params.set('search', val);
     } else {
       params.delete('search');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
