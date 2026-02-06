52a99f4bd23373990683d5a463f4282c426cdefa
magiclz233 <magiclz233@163.com>
2026-02-06 17:35:42 +0800

    feat(字体): 添加 Noto Serif SC 字体配置
    添加 Noto Serif SC 字体以支持中文衬线字体显示，提供更多排版选择。

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
 import {
   Lora,
   Noto_Sans_SC,
+  Noto_Serif_SC,
   Open_Sans,
 } from 'next/font/google';
 
@@ -15,6 +16,13 @@ export const notoSansSC = Noto_Sans_SC({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
   variable: '--font-sans-sc',
+  display: 'swap',
+});
+
+export const notoSerifSC = Noto_Serif_SC({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-serif-sc',
   display: 'swap',
 });
 
