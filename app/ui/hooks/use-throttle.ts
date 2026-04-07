81033563d6e594ec7215d7a378ffcb05c5513443
magiclz233 <magiclz233@163.com>
2026-04-07 16:29:44 +0800

    feat: 新增 useThrottle 自定义 Hook
    基于 use-debounce 库实现一个节流功能，用于限制值更新的频率。

---
+++ app/ui/hooks/use-throttle.ts
@@ -0,0 +1,6 @@
+import { useDebounce } from 'use-debounce';
+
+export function useThrottle<T>(value: T, delay = 100): T {
+  const [throttledValue] = useDebounce(value, delay, { maxWait: delay });
+  return throttledValue;
+}
